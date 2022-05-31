ece16305706e350811dc31d67296ff4d18886fa3
nimit2801 <mnimitsavant@outlook.com>
2022-06-01 03:35:06 +0530

    Made a prototype ✅

---
+++ public/script.js
@@ -0,0 +1,75 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined,   {
+    secure: true,
+    host: "3001-peers-peerjsserver-iokstn1x4gt.ws-us46.gitpod.io",
+    port: '443',
+    config: {'iceServers': [
+        { url: 'stun:stun.l.google.com:19302' },
+        { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
+    ]} /* Sample servers, please use appropriate ones */
+})
+
+
+const myVideo = document.createElement('video')
+myVideo.mute = true
+myVideo.onClick = () => {
+    console.log("clicked")
+}
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+    console.log("call ke aage hi nai aaya")
+    socket.on('user-connected', userId => {
+        console.log('User Connected ' + userId)
+        connectToNewUser(userId, stream)
+    })
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+socket.on('user-connected', userId => {
+    console.log('User Connected ' + userId)
+})
+
+socket.on('user-disconnected', userId => {
+    console.log("user disconnected: " + userId)
+    if(peers[userId])  peers[userId].close()
+})
+
+function connectToNewUser(userId, stream) {
+    console.log('User Connected ' + userId)
+    
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.appendChild(video)
+    console.log("playing your video now!")
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const {v4: uuidV4} = require('uuid')
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server);
+
+// Socket Connection and logic
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userID) =>{
+    console.log(`${userID} joined this ${roomId}`)
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userID)
+  
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userID)
+    })
+  }
+
+)})
+
+
+// EJS setup
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(cors({
+  origin: 'https://3001-peers-peerjsserver-iokstn1x4gt.ws-us46.gitpod.io'
+}))
+
+//Routes Setup
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', {roomId: req.params.room})
+})
+
+  
+
+
+server.listen(3000, () => {
+  console.log('listening on http://localhost:3000');
+});
